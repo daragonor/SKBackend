0f08ddbe1df6831e223525014a12bdce363e0597
Daniel <danie.aragon.0235@hotmail.com>
2019-08-31 18:48:49 -0500

    access allow cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const express = require('express');
-
+const cors = require('cors');
 // Intializations
 const app = express();
 
@@ -9,7 +9,14 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 // Middlewares
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin, Cache-Control");
+    // res.json({data: [1,2,3,4]})
+    next()
+});
 app.use(express.json());
+app.use(cors());
 
 //Routes
 app.use('/api', require('./routes/ingredients-route'));
