c3d7d139486fe53e62e90aef05ebe3f89461682b
Johnny Ramos <johnny.gerardo24@hotmail.com>
2019-11-23 04:16:18 -0500

    tendencias 3

--- src/routes/recetas-route.js
+++ src/routes/recetas-route.js
@@ -5,6 +5,15 @@ var router = express.Router();
 var repository = require('../repository/recetas-repository');
 var dbConn = require('../db');
 
+router.get('/', (req, res) => {
+    var id = Math.floor(Math.random() * 7) + 1;
+    dbConn.query('SELECT * FROM receta WHERE id = ?', [id], (error, result, fields) => {
+        if (error) throw error;
+        console.log(result);
+        return res.status(200).send({ error: false, data: result, message: 'Receta random' });
+    });
+});
+
 router.get('/:id/ingredientes', (req, res) => {
     var id = req.params.id;
     dbConn.query('SELECT t2.nombre, t1.cantidad, t1.medida FROM ingrediente_receta t1 JOIN ingrediente t2 ON t1.ingrediente_id = t2.id WHERE t1.receta_id = ?', [id], (error, result, fields) => {
